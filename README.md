c3160a6eb0c0a8ad0031efdc8544d01a0481d45e
Claire Annan <claire.annan.me@gmail.com>
2018-09-07 01:40:31 -0400

    Removing dependencies handled by composer. Adding theme files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jay's Top Modules
+This project extends the [drupal-composer/drupal-project](https://github.com/drupal-composer/drupal-project) by adding modules from the popular "Top modules" blog series on [Mediacurrent.com](Mediacurrent.com). Most of the instructions from that project [readme](https://github.com/drupal-composer/drupal-project) apply here as well.
+
+This project is meant as an educational tool that will save you time getting setup.
+
+# Installing
+The easiest way to get started is to clone this repo and run `composer install`. This will download Drupal core, recommended "Top modules" and their dependencies. Once that is done you can install Drupal as normal but now you will have additional contributed modules you can install. 
---
+++ web/themes/cannanso/js/cannanso.js
@@ -0,0 +1,130 @@
+var gridCells = {
+	anchor: '',
+	expander: '',
+	setPage: function(url) {
+		$.ajax({
+		    url: url,
+		    type: 'GET',
+		    dataType: 'html',
+		    timeout: 5000
+		})
+	  .done(function (result) {
+	      gridCells.update(result);
+	  })
+	  .fail(function () {
+	      gridCells.update("Error! Please refresh the page!");
+	  });
+	},
+	update: function(content) {
+		this.close();
+		this.expander = $('<div id="expander"></div>').appendTo($(this.anchor).parent());
+		this.expander.html(content);
+		this.expander.prepend('<a href="#" class="close">Close <i class="fa fa-times-circle-o"></i></a>');
+		this.expand();
+	},
+	expand: function() {    
+    var activeCell = $(this.anchor).parent(), 
+    		expander = this.expander, 
+    		newHeight = this.setHeights(activeCell, expander);
+
+    activeCell.addClass('expanded');
+
+    //Only set a height on the active cell for wider screens
+    if(window.innerWidth >= 768){
+    	activeCell.css({'height': newHeight});
+    }
+    expander.slideDown(400);
+	},
+	setHeights: function(a,b) {
+		var marginHeight = 16;		
+		return a.outerHeight(true) + b.outerHeight(true) - marginHeight;
+	},
+	positionPreview: function() {
+		//scrollTop
+		console.log('scroll to position the preview in the right place');
+	},
+	close: function(){
+    $('.grid-cell.expanded').removeClass('expanded').css({'height': 'auto'});
+    $('#expander').slideUp(400);
+    $('#expander').remove();
+	},
+	init: function() {
+	  $('.grid-cell a').on('click', function(e) {
+	  	e.preventDefault();
+			gridCells.anchor = this;
+			gridCells.setPage(this.getAttribute('href'));
+	  })
+	  .mouseenter(function(e){
+	    e.preventDefault();
+	    $(this).children('.caption').slideDown('slow');
+	  })
+	  .mouseleave(function(e){
+	    e.preventDefault();
+	    $(this).children('.caption').slideUp('slow');  
+	  });
+	  
+	  $('.grid-cell').on('click', ' .close', function(e){
+	    e.preventDefault();
+	    gridCells.close();  
+	  });
+	}
+};
+
+var slideItem = function() {
+	var itemHeight = $('#testimonials .item').outerHeight();
+	$('#testimonials .wrapper').css('height', itemHeight);
+
+ 	$('#testimonials .next').click(function(){
+ 		var activeItem = $('#testimonials .active'),
+ 				nextItem = activeItem.next();
+
+ 		if(nextItem.length === 0) {
+ 			nextItem = $('.item').first();
+ 		}
+ 		activeItem.fadeOut(600).removeClass('active');
+ 		nextItem.fadeIn(600).addClass('active');
+ 	});
+
+ 	$('#testimonials .prev').click(function(){
+ 		var activeItem = $('#testimonials .active'),
+ 				prevItem = activeItem.prev();
+
+ 		if(prevItem.length === 0) {
+ 			prevItem = $('.item').last();
+ 		}
+ 		activeItem.fadeOut(600).removeClass('active');
+ 		prevItem.fadeIn(600).addClass('active');
+ 	}); 	
+
+};
+
+var contactVal = function() {
+	$('#contact form').on('submit', function() {		
+		$('.form-control').each(function() {
+			if (!$(this).val().length) {
+				$(this).css('border-color', 'red');
+			} 
+		})
+		.blur(function() {
+			if ($(this).val().length) {
+				$(this).css('border-color', 'green');
+			}
+		});	
+	});
+};
+
+
+(function ($, Drupal) {
+	console.log('inside');	// this works
+
+	$(window).resize(function() {
+	  gridCells.close();
+	});	
+
+	gridCells.init();
+	slideItem();
+	contactVal();
+
+	$('#intro i').after('<span>line of code</span>');
+
+})(jQuery, Drupal);
