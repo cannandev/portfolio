4f85da1a2d8c6a43ab3a47f7df1e983192facdfe
Claire Annan <claire.annan.me@gmail.com>
2018-10-23 00:47:44 -0400

    Adding custom contact form module.

---
+++ web/modules/custom/cannanso_contact/src/Form/SiteContactForm.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cbearhoney
+ * Date: 10/22/18
+ * Time: 10:18 PM
+ */
---
+++ web/modules/custom/cannanso_contact/src/Plugin/Block/SiteContactFormBlock.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cbearhoney
+ * Date: 10/23/18
+ * Time: 12:40 AM
+ */
