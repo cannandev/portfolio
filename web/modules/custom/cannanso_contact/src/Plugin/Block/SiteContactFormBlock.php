9a3f9136ecb088861023c88bc14340268b4369d7
Claire Annan <claire.annan.me@gmail.com>
2018-10-23 00:49:13 -0400

    Adding custom contact form module.

--- web/modules/custom/cannanso_contact/src/Plugin/Block/SiteContactFormBlock.php
+++ web/modules/custom/cannanso_contact/src/Plugin/Block/SiteContactFormBlock.php
@@ -5,3 +5,49 @@
  * Date: 10/23/18
  * Time: 12:40 AM
  */
+
+namespace Drupal\cannanso_contact\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class SiteContactFormBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  /**
+   * Creates an instance of the plugin.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The container to pull out services used in the plugin.
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   *
+   * @return static
+   *   Returns an instance of this plugin.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    // TODO: Implement create() method.
+  }
+
+  /**
+   * Builds and returns the renderable array for this block plugin.
+   *
+   * If a block should not be rendered because it has no content, then this
+   * method must also ensure to return no content: it must then only return an
+   * empty array, or an empty array with #cache set (with cacheability metadata
+   * indicating the circumstances for it being empty).
+   *
+   * @return array
+   *   A renderable array representing the content of the block.
+   *
+   * @see \Drupal\block\BlockViewBuilder
+   */
+  public function build() {
+    // TODO: Implement build() method.
+  }
+
+
+}
